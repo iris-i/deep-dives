426edc22c944ef5f7077dd179e6c411f3559278e
Iris Ibekwe <iris.ibekwe@civicactions.com>
2023-08-28 05:55:55 -0400

    Add a page for drupal posts.

---
+++ src/app/drupal/page.js
@@ -0,0 +1,46 @@
+'use client'
+import Post from '../../components/Post'
+import { DrupalClient } from 'next-drupal'
+import { useState, useEffect } from 'react'
+
+const drupal = new DrupalClient('https://dev-deep-dives.pantheonsite.io', {
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+async function getPosts() {
+  const data = await drupal.getResourceCollection('node--post')
+  return data
+}
+
+export default function Page() {
+  let postsData = getPosts()
+  let [posts, setPosts] = useState(postsData)
+
+  useEffect(() => {
+    async function getPosts() {
+      const data = await drupal.getResourceCollection('node--post')
+      setPosts(data)
+    }
+    getPosts()
+  }, []
+  )
+
+  return (
+    <main>
+      <h2>Posts from Drupal</h2>
+      <ul>
+        {posts.length > 0 && posts.map((post) => {
+          console.log(post)
+          return (
+            <li key={post.id}>
+              <h3>Title: {post.title} </h3>
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
+}
